6bff208215aa43154add7fbf3d800732c02df384
juliane157 <juliane.javorski@escola.pr.gov.br>
2025-08-01 14:55:37 -0300

    ,

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-function inserirNome(){}
+function inserirNome(){
+let nomeUsuario = prompt("Qual o seu nome");
+   let elemento = document.querySelector("#nome-usuario");
+   console.log(elemento);
+   elemento.textContent = nomeUsuario;
+}
+
+ const lista=Document.querySelector('Wlista');
