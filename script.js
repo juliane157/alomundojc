a58305408d28afa7615264b22e7d51ee954408e6
juliane157 <juliane.javorski@escola.pr.gov.br>
2025-08-01 15:09:44 -0300

    ;;

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ let nomeUsuario = prompt("Qual o seu nome");
 
  const lista=Document.querySelector('Wlista');
 
- lista.textContent=linguagens(0)
- lista.textContent=linguagens(1)
- lista.textContent=linguagens(2)
+ lista.textContent=linguagens[0]
+ lista.textContent=linguagens[1]
+ lista.textContent=linguagens[3]
+ let aluno1 = { 
+     nom: 'Juliane', 
+     idade: 18, 
+     anoletivo:'terceiroensinomédio',
+     Materiasfavoritas:['Português']
+ }
