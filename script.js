f468e9283089229602dceb2f5ac72c69f56c924d
juliane157 <juliane.javorski@escola.pr.gov.br>
2025-08-06 13:17:46 -0300

    ;

--- script.js
+++ script.js
@@ -18,6 +18,10 @@ let nomeUsuario = prompt("Qual o seu nome");
  }
 
  console.log(aluno1.nome)
- console.log(aluno1.nome)
+ console.log(aluno1.idade)
  console.log(aluno1.materiasFavoritas)
  console.log(aluno1.materiasFavoritas[1])
+ const objeto=document.querySelector('#aluno');
+ objeto.textContent=aluno1.nome
+ objeto.textContent=aluno1.idade
+ objeto.textContent=aluno1.materiasFavoritas[1]
